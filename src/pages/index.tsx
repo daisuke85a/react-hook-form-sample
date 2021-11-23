355d6017cfa9cca52173c1bf2d96e13a00b16145
Daisuke Saito <dsaito85a@gmail.com>
2021-11-23 23:00:34 +0900

    [Add] tailwindの導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <>Hello Next!</>;
+  return <p className="text-lg text-red-500">Hello Next!</p>;
 };
 
 export default Home;
